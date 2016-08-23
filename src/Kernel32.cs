d9327f4d48c91bd4fe4fec819e6daf036ec12d19
ghbvft6 <ghbvft6@gmail.com>
2016-08-23 19:44:55 +0200

    added OpenProcess() and ReadProcessMemory()

--- src/Kernel32.cs
+++ src/Kernel32.cs
@@ -11,9 +11,19 @@ namespace csgop.src {
             [DllImport("kernel32.dll", SetLastError = true)]
             [return: MarshalAs(UnmanagedType.Bool)]
             public static extern bool AllocConsole();
+            [DllImport("kernel32.dll")]
+            public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+            [DllImport("kernel32.dll")]
+            public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out uint lpNumberOfBytesRead);
         }
         public bool AllocConsole() {
             return Static.AllocConsole();
+        }
+        public IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId) {
+            return Static.OpenProcess(dwDesiredAccess, bInheritHandle, dwProcessId);
+        }
+        public bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out uint lpNumberOfBytesRead) {
+            return Static.ReadProcessMemory(hProcess, lpBaseAddress, lpBuffer, nSize, out lpNumberOfBytesRead);
         }
     }
 }
