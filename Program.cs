014534e4ad4a6c55fa5afc63afbb60ec89048e3c
ghbvft6 <ghbvft6@gmail.com>
2016-08-27 16:38:58 +0200

    fixed Form blocking CSGOClient thread

--- Program.cs
+++ Program.cs
@@ -14,10 +14,12 @@ namespace csgop {
         static void Main() {
             var kernel = Kernel32.Instance;
             kernel.AllocConsole();
+
+            new Thread(new CSGOClient().Run).Start();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            new Thread(new CSGOClient().Run).Start();
         }
     }
 }
