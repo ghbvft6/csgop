9148fc57269c53f4f55a0e4b4efe65ed9f451741
leopordz <leopordz@users.noreply.github.com>
2016-12-13 15:55:57 +0100

    updated offsets

--- Games/CSGO/Data/Client.cs
+++ Games/CSGO/Data/Client.cs
@@ -7,9 +7,9 @@ namespace CSGOP.Games.CSGO.Data {
     unsafe class Client : Client<Process> {
 
         public Client(Func<IntPtr> GetBaseAddress) {
-            player = new Player(GetBaseAddress, 0xAA38E4);
-            players = External.NewArray(24, (i) => new Player(GetBaseAddress, 0x4AC5E94 + (i + 1) * 0x10));
-            view = new External<float>.Array(16, GetBaseAddress, 0x4AB7A34, sizeof(float));
+            player = new Player(GetBaseAddress, 0xAA68E4);
+            players = External.NewArray(24, (i) => new Player(GetBaseAddress, 0x4AC9224 + (i + 1) * 0x10));
+            view = new External<float>.Array(16, GetBaseAddress, 0x4ABADC4, sizeof(float));
         }
     }
 }
