4a7082b37ca881e5ab48416e8f460191d63406b3
ghbvft6 <ghbvft6@gmail.com>
2017-07-30 14:09:49 +0200

    renamed parentObject to playerBase for Bones

--- Games/CSGO/Data/Bones.cs
+++ Games/CSGO/Data/Bones.cs
@@ -5,8 +5,8 @@ using System;
 namespace CSGOP.Games.CSGO.Data {
     unsafe class Bones : Bones<Process> {
 
-        public Bones(External<IntPtr, Process> parentObject, int offset) {
-            var boneBase = new External<IntPtr>(parentObject, offset);
+        public Bones(External<IntPtr, Process> playerBase, int offset) {
+            var boneBase = new External<IntPtr>(playerBase, offset);
             head = new BonesVector(boneBase, 0x30 * 8);
             somethingelse = new BonesVector(boneBase, 0x30 * 5);
         }
